54329ccc8dfcd41e0aaeda85e6a5803bf62d7008
Ard Biesheuvel <ardb@kernel.org>
2021-02-03 12:36:18 +0100

    arm64: assembler: add cond_yield macro
    Add a macro cond_yield that branches to a specified label when called if
    the TIF_NEED_RESCHED flag is set and decreasing the preempt count would
    make the task preemptible again, resulting in a schedule to occur. This
    can be used by kernel mode SIMD code that keeps a lot of state in SIMD
    registers, which would make chunking the input in order to perform the
    cond_resched() check from C code disproportionately costly.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>
    Link: https://lore.kernel.org/r/20210203113626.220151-2-ardb@kernel.org
    Signed-off-by: Will Deacon <will@kernel.org>

--- arch/arm64/include/asm/assembler.h
+++ arch/arm64/include/asm/assembler.h
@@ -733,4 +733,20 @@ USER(\label, ic	ivau, \tmp2)			// invalidate I line PoU
 .Lyield_out_\@ :
 	.endm
 
-#endif	/* __ASM_ASSEMBLER_H */
+	/*
+	 * Check whether preempt-disabled code should yield as soon as it
+	 * is able. This is the case if re-enabling preemption a single
+	 * time results in a preempt count of zero, and the TIF_NEED_RESCHED
+	 * flag is set. (Note that the latter is stored negated in the
+	 * top word of the thread_info::preempt_count field)
+	 */
+	.macro		cond_yield, lbl:req, tmp:req
+#ifdef CONFIG_PREEMPTION
+	get_current_task \tmp
+	ldr		\tmp, [\tmp, #TSK_TI_PREEMPT]
+	sub		\tmp, \tmp, #PREEMPT_DISABLE_OFFSET
+	cbz		\tmp, \lbl
+#endif
+	.endm
+
+#endif /* __ASM_ASSEMBLER_H */
