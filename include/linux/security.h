c3b84cdc67275c8eddfc2472c540632a94f5baf0
Vivek Goyal <vgoyal@redhat.com>
2021-10-12 09:23:07 -0400

    security: Return xattr name from security_dentry_init_security()
    Right now security_dentry_init_security() only supports single security
    label and is used by SELinux only. There are two users of this hook,
    namely ceph and nfs.
    
    NFS does not care about xattr name. Ceph hardcodes the xattr name to
    security.selinux (XATTR_NAME_SELINUX).
    
    I am making changes to fuse/virtiofs to send security label to virtiofsd
    and I need to send xattr name as well. I also hardcoded the name of
    xattr to security.selinux.
    
    Stephen Smalley suggested that it probably is a good idea to modify
    security_dentry_init_security() to also return name of xattr so that
    we can avoid this hardcoding in the callers.
    
    This patch adds a new parameter "const char **xattr_name" to
    security_dentry_init_security() and LSM puts the name of xattr
    too if caller asked for it (xattr_name != NULL).
    
    Signed-off-by: Vivek Goyal <vgoyal@redhat.com>
    Reviewed-by: Jeff Layton <jlayton@kernel.org>
    Reviewed-by: Christian Brauner <christian.brauner@ubuntu.com>
    Acked-by: James Morris <jamorris@linux.microsoft.com>
    [PM: fixed typos in the commit description]
    Signed-off-by: Paul Moore <paul@paul-moore.com>

--- include/linux/security.h
+++ include/linux/security.h
@@ -248,8 +248,9 @@ int security_sb_clone_mnt_opts(const struct super_block *oldsb,
 int security_add_mnt_opt(const char *option, const char *val,
 				int len, void **mnt_opts);
 int security_dentry_init_security(struct dentry *dentry, int mode,
-					const struct qstr *name, void **ctx,
-					u32 *ctxlen);
+				  const struct qstr *name,
+				  const char **xattr_name, void **ctx,
+				  u32 *ctxlen);
 int security_dentry_create_files_as(struct dentry *dentry, int mode,
 					struct qstr *name,
 					const struct cred *old,
@@ -618,6 +619,7 @@ static inline void security_inode_free(struct inode *inode)
 static inline int security_dentry_init_security(struct dentry *dentry,
 						 int mode,
 						 const struct qstr *name,
+						 const char **xattr_name,
 						 void **ctx,
 						 u32 *ctxlen)
 {
