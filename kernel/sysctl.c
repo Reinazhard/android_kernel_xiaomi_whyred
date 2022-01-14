128c703d684e8d52bc88788c7b37c7b32162d8d7
Reinazhard <reinazhard@gmail.com>
2022-01-14 20:16:58 +0100

    kernel: sched: Checkout to xiaomi gourami
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -616,7 +616,6 @@ static struct ctl_table kern_table[] = {
 		.extra1		= &zero,
 		.extra2		= &one_thousand,
 	},
-#endif
 	{
 		.procname	= "sched_force_lb_enable",
 		.data		= &sysctl_sched_force_lb_enable,
@@ -626,6 +625,7 @@ static struct ctl_table kern_table[] = {
 		.extra1		= &zero,
 		.extra2		= &one,
 	},
+#endif
 #ifdef CONFIG_SCHED_DEBUG
 	{
 		.procname       = "sched_cstate_aware",
