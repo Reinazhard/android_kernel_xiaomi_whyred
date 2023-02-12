07504e068ab8a00741cefe28b9fcd14094406003
Reinazhard <reinazhard@gmail.com>
2023-02-12 13:23:13 +0100

    Revert "devfreq: suppress platform driver bind / unbind feature"
    This reverts commit d548f6c37de5fa324df0c0c55105df6fa1ffd86a.

--- drivers/devfreq/arm-memlat-mon.c
+++ drivers/devfreq/arm-memlat-mon.c
@@ -354,7 +354,6 @@ static struct platform_driver arm_memlat_mon_driver = {
 	.driver = {
 		.name = "arm-memlat-mon",
 		.of_match_table = memlat_match_table,
-		.suppress_bind_attrs = true,
 	},
 };
 
