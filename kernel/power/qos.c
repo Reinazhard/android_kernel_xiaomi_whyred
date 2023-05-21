b7844164011b391839113b570b512efbfb7ceb74
Reinazhard <reinazhard@gmail.com>
2023-05-21 21:41:27 +0200

    te
    Signed-off-by: Reinazhard <reinazhard@gmail.com>

--- kernel/power/qos.c
+++ kernel/power/qos.c
@@ -550,9 +550,9 @@ static void pm_qos_irq_release(struct kref *ref)
 	struct pm_qos_constraints *c =
 				pm_qos_array[req->pm_qos_class]->constraints;
 
-	spin_lock_irqsave(&pm_qos_lock, flags);
+	raw_spin_lock(&pm_qos_lock);
 	cpumask_setall(&req->cpus_affine);
-	spin_unlock_irqrestore(&pm_qos_lock, flags);
+	raw_spin_unlock(&pm_qos_lock);
 
 	pm_qos_update_target(c, &req->node, PM_QOS_UPDATE_REQ,
 			c->default_value, false);
@@ -571,13 +571,13 @@ static void pm_qos_irq_notify(struct irq_affinity_notify *notify,
 			irq_data_get_effective_affinity_mask(&desc->irq_data);
 	bool affinity_changed = false;
 
-	spin_lock_irqsave(&pm_qos_lock, flags);
+	raw_spin_lock(&pm_qos_lock);
 	if (!cpumask_equal(&req->cpus_affine, new_affinity)) {
 		cpumask_copy(&req->cpus_affine, new_affinity);
 		affinity_changed = true;
 	}
 
-	spin_unlock_irqrestore(&pm_qos_lock, flags);
+	raw_spin_unlock(&pm_qos_lock);
 
 	if (affinity_changed)
 		pm_qos_update_target(c, &req->node, PM_QOS_UPDATE_REQ,
