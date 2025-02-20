870c6e5c0b477df670ad3863a6b0869ba2c6a0fa
M. "Harumajati" Alfarozi <reinazhard@gmail.com>
2025-02-20 15:17:05 +0700

    Fix compilation on scheduler checkout
    Signed-off-by: M. "Harumajati" Alfarozi <reinazhard@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -624,7 +624,6 @@ static struct ctl_table kern_table[] = {
 		.extra1		= &zero,
 		.extra2		= &one_thousand,
 	},
-#endif
 	{
 		.procname	= "sched_force_lb_enable",
 		.data		= &sysctl_sched_force_lb_enable,
@@ -634,6 +633,7 @@ static struct ctl_table kern_table[] = {
 		.extra1		= &zero,
 		.extra2		= &one,
 	},
+#endif
 #ifdef CONFIG_SCHED_DEBUG
 	{
 		.procname       = "sched_cstate_aware",
